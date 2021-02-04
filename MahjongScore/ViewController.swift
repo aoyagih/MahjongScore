8274369233791321b9502d39f3011a77cf71c3ae
aoyagih <hiroki3290@gmail.com>
2021-02-05 02:23:15 +0900

    Initial Commit

---
+++ MahjongScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MahjongScore
+//
+//  Created by Aoyagi Hiroki on 2021/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
